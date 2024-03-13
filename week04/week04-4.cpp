e8c7437c75ccad27f86acb1000803f8e477cf6f5
yuan04100517 <yuan04100517@gmail.com>
2024-03-13 11:55:46 +0800

    week04

---
+++ week04/week04-4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 3;
+    while(a>0)
+    {
+        printf("a是%d\n", a);
+        a--;
+    }
+
+    for (int b=3; b>0; b--){
+        printf("b是%d\n", b);
+    }
+}
+
