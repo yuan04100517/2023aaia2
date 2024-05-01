c65d583b2133b82607c1e5e65c288aacaabe6deb
unknown <yuan04100517>
2024-05-01 12:04:26 +0800

    week11

---
+++ week11/week11-3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int gcd(int a, int b){
+    if(a==0) return b;
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+int main()
+{
+    printf("請輸入2個數字: ");
+    int a, b;
+    scanf("%d %d", &a,&b);
+    int ans = gcd(a, b);
+    printf("答案是:%d", ans);
+}
+
