53d6e87d2c3f18551251b5db0be53894e4a9fb1f
yuan04100517 <yuan04100517@gmail.com>
2024-05-22 11:35:16 +0800

    week14

---
+++ week14/week14-3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a= 5;
+
+    if(999) printf("999成立\n");
+    if(3) printf("3成立\n");
+    if(2) printf("2成立\n");
+    if(1) printf("1成立\n");
+    if(0) printf("請問0成立嗎?\n");
+    if(-1) printf("-1成立\n");
+    if(-2) printf("-2成立\n");
+    if(-3) printf("-3成立\n");
+}
